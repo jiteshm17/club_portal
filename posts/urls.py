d8d1ef8a7511af9290248051711b1a256795fce2
BrijeshBumrela <brijeshbumrela@gmail.com>
2018-11-24 19:08:42 +0530

    Added static files

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = "posts"
 
 urlpatterns = [
-    
+    path('club-list', views.clublist, name='club_list')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def clublist(request):
+    return render(request, 'posts/club_list.html')
